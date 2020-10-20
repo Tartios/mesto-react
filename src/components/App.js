72eca36ebeddcaee742db6e98b52557ef6945813
Tartios <tartios@yandex.ru>
2020-10-20 08:13:16 +0300

    new context

--- src/components/App.js
+++ src/components/App.js
@@ -5,8 +5,37 @@ import Main from "./Main.js";
 import Footer from "./Footer.js";
 import PopupWithForm from "./PopupWithForm.js";
 import ImagePopup from "./ImagePopup.js";
+import { api } from "../utils/Api.js";
+import { CurrentUserContext } from "../contexts/CurrentUserContext.js";
+import { arrCards } from "../contexts/CardsContext.js";
 
 function App() {
+  const [currentUser, setCurrentUser] = React.useState([]);
+
+  React.useEffect(() => {
+    api
+      .getUserInfo()
+      .then((res) => {
+        setCurrentUser(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
+  const [cards, setNewCards] = React.useState([]);
+
+  React.useEffect(() => {
+    api
+      .getInitialCards()
+      .then((res) => {
+        setNewCards(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
   function handleEditAvatarClick() {
     setIsEditAvatarPopupOpen(true);
   }
@@ -42,98 +71,102 @@ function App() {
 
   return (
     <div className="page">
-      <PopupWithForm
-        title="Обновить аватар"
-        name="avatar"
-        children=""
-        isOpen={isEditAvatarPopupOpen}
-        onClose={closeAllPopups}
-        buttonText="Сохранить"
-      >
-        <input
-          name="link"
-          type="url"
-          id="link-input"
-          className="popup__input popup__input_link"
-          placeholder="Ссылка на аватар"
-          required
-        />
-        <span id="link-input-error" className="popup__input-error"></span>
-      </PopupWithForm>
+      <CurrentUserContext.Provider value={currentUser}>
+        <arrCards.Provider value={cards}>
+        <PopupWithForm
+          title="Обновить аватар"
+          name="avatar"
+          children=""
+          isOpen={isEditAvatarPopupOpen}
+          onClose={closeAllPopups}
+          buttonText="Сохранить"
+        >
+          <input
+            name="link"
+            type="url"
+            id="link-input"
+            className="popup__input popup__input_link"
+            placeholder="Ссылка на аватар"
+            required
+          />
+          <span id="link-input-error" className="popup__input-error"></span>
+        </PopupWithForm>
 
-      <PopupWithForm
-        title="Вы уверены?"
-        name="card-delete"
-        buttonText="Да"
-      ></PopupWithForm>
+        <PopupWithForm
+          title="Вы уверены?"
+          name="card-delete"
+          buttonText="Да"
+        ></PopupWithForm>
 
-      <PopupWithForm
-        title="Редактировать профиль"
-        name="profile"
-        isOpen={isEditProfilePopupOpen}
-        onClose={closeAllPopups}
-        buttonText="Сохранить"
-      >
-        <input
-          name="name"
-          type="text"
-          id="name-input"
-          className="popup__input popup__input_name"
-          minLength="2"
-          maxLength="40"
-          required
-        />
-        <span id="name-input-error" className="popup__input-error"></span>
-        <input
-          name="about"
-          type="text"
-          id="self-input"
-          className="popup__input popup__input_prof"
-          minLength="2"
-          maxLength="200"
-          required
-        />
-        <span id="self-input-error" className="popup__input-error"></span>
-      </PopupWithForm>
+        <PopupWithForm
+          title="Редактировать профиль"
+          name="profile"
+          isOpen={isEditProfilePopupOpen}
+          onClose={closeAllPopups}
+          buttonText="Сохранить"
+        >
+          <input
+            name="name"
+            type="text"
+            id="name-input"
+            className="popup__input popup__input_name"
+            minLength="2"
+            maxLength="40"
+            required
+          />
+          <span id="name-input-error" className="popup__input-error"></span>
+          <input
+            name="about"
+            type="text"
+            id="self-input"
+            className="popup__input popup__input_prof"
+            minLength="2"
+            maxLength="200"
+            required
+          />
+          <span id="self-input-error" className="popup__input-error"></span>
+        </PopupWithForm>
 
-      <PopupWithForm
-        title="Новое место"
-        name="add"
-        isOpen={isAddPlacePopupOpen}
-        onClose={closeAllPopups}
-        buttonText="Создать"
-      >
-        {" "}
-        <input
-          name="name"
-          type="text"
-          id="mark-input"
-          className="popup__input popup__input_mark"
-          placeholder="Название"
-          minLength="1"
-          maxLength="30"
-          required
+        <PopupWithForm
+          title="Новое место"
+          name="add"
+          isOpen={isAddPlacePopupOpen}
+          onClose={closeAllPopups}
+          buttonText="Создать"
+        >
+          {" "}
+          <input
+            name="name"
+            type="text"
+            id="mark-input"
+            className="popup__input popup__input_mark"
+            placeholder="Название"
+            minLength="1"
+            maxLength="30"
+            required
+          />
+          <span id="mark-input-error" className="popup__input-error"></span>
+          <input
+            name="link"
+            type="url"
+            id="link-input"
+            className="popup__input popup__input_link"
+            placeholder="Ссылка на картинку"
+            required
+          />
+          <span id="link-input-error" className="popup__input-error"></span>
+        </PopupWithForm>
+        <ImagePopup card={selectedCard} onClose={closeAllPopups} />
+        <Header />
+        <Main
+          onEditProfile={handleEditProfileClick}
+          onEditAvatar={handleEditAvatarClick}
+          onAddPlace={handleAddPlaceClick}
+          onCardClick={handleCardClick}
         />
-        <span id="mark-input-error" className="popup__input-error"></span>
-        <input
-          name="link"
-          type="url"
-          id="link-input"
-          className="popup__input popup__input_link"
-          placeholder="Ссылка на картинку"
-          required
-        />
-        <span id="link-input-error" className="popup__input-error"></span>
-      </PopupWithForm>
-      <ImagePopup card={selectedCard} onClose={closeAllPopups} />
-      <Header />
-      <Main
-        onEditProfile={handleEditProfileClick}
-        onEditAvatar={handleEditAvatarClick}
-        onAddPlace={handleAddPlaceClick}
-        onCardClick={handleCardClick}
-      />
-      <Footer />
+        <Footer />
+        </arrCards.Provider>
+      </CurrentUserContext.Provider>
     </div>
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,51 +1,56 @@
 import React from "react";
 import { api } from "../utils/Api.js";
 import Card from "./Card.js";
+import { CurrentUserContext } from "../contexts/CurrentUserContext.js";
+import { arrCards } from "../contexts/CardsContext.js";
 
 function Main({ onEditProfile, onAddPlace, onEditAvatar, onCardClick }) {
-  const [userName, setUserName] = React.useState("");
+  // const [userName, setUserName] = React.useState("");
 
-  const [userDescription, setUserDescription] = React.useState("");
+  // const [userDescription, setUserDescription] = React.useState("");
 
-  const [userAvatar, setUserAvatar] = React.useState("");
+  // const [userAvatar, setUserAvatar] = React.useState("");
 
-  const [cards, setNewCards] = React.useState([]);
+  // const [cards, setNewCards] = React.useState([]);
 
-  React.useEffect(() => {
-    api
-      .getUserInfo()
-      .then((res) => {
-        setUserName(res.name);
-        setUserDescription(res.about);
-        setUserAvatar(res.avatar);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
+  const currentUser = React.useContext(CurrentUserContext);
 
-  React.useEffect(() => {
-    api
-      .getInitialCards()
-      .then((res) => {
-        setNewCards(res);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
+  const cards = React.useContext(arrCards);
+  // React.useEffect(() => {
+  //   api
+  //     .getUserInfo()
+  //     .then((res) => {
+  //       setUserName(res.name);
+  //       setUserDescription(res.about);
+  //       setUserAvatar(res.avatar);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // }, []);
+
+  // React.useEffect(() => {
+  //   api
+  //     .getInitialCards()
+  //     .then((res) => {
+  //       setNewCards(res);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // }, []);
   return (
     <main>
       <section className="profile">
         <button
-          style={{ backgroundImage: `url(${userAvatar})` }}
+          style={{ backgroundImage: `url(${currentUser.avatar})` }}
           className="profile__avatar"
           onClick={onEditAvatar}
         ></button>
         <div className="profile__info">
           <div className="profile__info-input">
-            <h1 className="profile__name">{userName}</h1>
-            <p className="profile__prof">{userDescription}</p>
+            <h1 className="profile__name">{currentUser.name}</h1>
+            <p className="profile__prof">{currentUser.about}</p>
           </div>
           <button
             type="button"
